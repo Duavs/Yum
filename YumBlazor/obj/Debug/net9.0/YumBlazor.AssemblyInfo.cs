d29f8ec36739696a3801a3e8f2d78d836c22c798
John Duavis <paulduavis09@gmail.com>
2025-07-15 22:42:55 +0800

    created Modal component

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db4cdc8aee5b5afd6444806e737b7766731af089")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54376f963d9dcde1fc9a5b95ed318a97158813b")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

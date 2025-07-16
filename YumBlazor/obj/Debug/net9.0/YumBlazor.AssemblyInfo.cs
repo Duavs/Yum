873051bb1420ad5638ec3c9f9e4296f6f72f1956
John Duavis <paulduavis09@gmail.com>
2025-07-16 23:12:30 +0800

    changes

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29f8ec36739696a3801a3e8f2d78d836c22c798")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f3d796f166f7dc3f8d3b31c63345a88147a524")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

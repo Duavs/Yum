91ca134a3b3793c37526d51898888592f6e82748
John Duavis <paulduavis09@gmail.com>
2025-07-20 17:21:36 +0800

    added image deletion functionality in ProductRepository to remove associated files on product deletion

--- YumBlazor/Repository/ProductRepository.cs
+++ YumBlazor/Repository/ProductRepository.cs
@@ -7,9 +7,12 @@ namespace YumBlazor.Repository;
 public class ProductRepository : IProductRepository
 {
     private readonly ApplicationDbContext _db;
-    public ProductRepository(ApplicationDbContext db)
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    public ProductRepository(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
     {
         _db = db;
+        _webHostEnvironment = webHostEnvironment;
+        
     }
     public async Task<Product>  CreateAsync(Product obj)
     {
@@ -40,6 +43,11 @@ public class ProductRepository : IProductRepository
     public async Task<bool>  DeleteAsync(int id)
     {
         var obj = await _db.Product.FirstOrDefaultAsync(u => u.Id == id);
+        var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, obj.ImageUrl.TrimStart('/'));
+        if (File.Exists(imagePath))
+        {
+            File.Delete(imagePath);
+        }
         if (obj != null)
         {
             _db.Product.Remove(obj);

5a85a069dc90a972a5f144e8dcd863391e006ba5
John Duavis <paulduavis09@gmail.com>
2025-07-20 15:43:22 +0800

    enhanced image upload functionality in ProductUpsert component with preview and improved file handling

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89468f1b66243e0f463a24c197dfeef0edb6b359")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b9c39233e73823261516763be5c1c3cc9b3df91")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

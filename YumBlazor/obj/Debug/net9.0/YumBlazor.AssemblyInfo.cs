1fafcaf98651e83517392129506c92858f8a8761
John Duavis <paulduavis09@gmail.com>
2025-07-21 23:08:48 +0800

    styled Register component for improved layout and user experience

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720177bd9ced7a18020654d13dae64e69b1e2431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+009b356bb7d4c93ce661088df2634447e8fa8703")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

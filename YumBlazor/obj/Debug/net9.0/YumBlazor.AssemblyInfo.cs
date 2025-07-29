8686cf7d4efe22c1070af36d60eb8968cb0ac634
John Duavis <paulduavis09@gmail.com>
2025-07-29 22:49:53 +0800

    adjust margin layout

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2fbaf73d632c3a7e12c3fdece9665aa15d26134")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8607c93d438c0f9da9a80fab61ddbc59cc27630")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

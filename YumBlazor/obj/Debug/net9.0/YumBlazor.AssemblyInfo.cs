f6a5852d09a365a210ea00801f0d26c9a7e874f8
John Duavis <paulduavis09@gmail.com>
2025-07-30 20:41:24 +0800

    populate all categories

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8607c93d438c0f9da9a80fab61ddbc59cc27630")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8686cf7d4efe22c1070af36d60eb8968cb0ac634")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

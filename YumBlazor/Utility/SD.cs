f7d54000d794f9f3b5a69e087b5b062138848a2d
John Duavis <paulduavis09@gmail.com>
2025-08-02 21:52:52 +0800

    added status for the item

--- YumBlazor/Utility/SD.cs
+++ YumBlazor/Utility/SD.cs
@@ -3,5 +3,10 @@ namespace YumBlazor.Utility;
 public static class SD
 {
   public static string Role_Admin = "Admin";
-  public static string Role_Customer = "Customer";  
+  public static string Role_Customer = "Customer";
+
+  public static string StatusPending = "Pending";
+  public static string StatusReadyForPickUp = "ReadyForPickUp";
+  public static string StatusCompleted = "Completed";
+  public static string StatusCancelled = "Cancelled";
 }

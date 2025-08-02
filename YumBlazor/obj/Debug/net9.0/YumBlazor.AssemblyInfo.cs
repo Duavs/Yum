74950bcebf00c2c6dceeca4f87f3fb6161835769
John Duavis <paulduavis09@gmail.com>
2025-08-02 09:58:01 +0800

    change currency to PHP

--- YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
+++ YumBlazor/obj/Debug/net9.0/YumBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba87dc62d48ccb4fd6b0b70343c278e158219c67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23801cac705d649fae063036aa5cafe1124b4a8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YumBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

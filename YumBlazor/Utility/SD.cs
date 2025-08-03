4bf2d002731310f1acf3068d89e78ba8fd0c18b1
John Duavis <paulduavis09@gmail.com>
2025-08-03 11:14:28 +0800

    added ConvertShoppingCartListToOrderDetails

--- YumBlazor/Utility/SD.cs
+++ YumBlazor/Utility/SD.cs
@@ -1,3 +1,5 @@
+using YumBlazor.Data;
+
 namespace YumBlazor.Utility;
 
 public static class SD
@@ -9,4 +11,21 @@ public static class SD
   public static string StatusReadyForPickUp = "ReadyForPickUp";
   public static string StatusCompleted = "Completed";
   public static string StatusCancelled = "Cancelled";
+  
+  public static List<OrderDetail> ConvertShoppingCartListToOrderDetails(List<ShoppingCart> shoppingCarts)
+  {
+    List<OrderDetail> orderDetails = new List<OrderDetail>();
+    foreach (var cart in shoppingCarts)
+    {
+      OrderDetail orderDetail = new OrderDetail()
+      {
+        ProductId = cart.ProductId,
+        Count = cart.Count,
+        Price = Convert.ToDouble(cart.Product.Price),
+        ProductName = cart.Product.Name
+      };
+      orderDetails.Add(orderDetail);
+    }
+    return orderDetails;
+  }
 }
